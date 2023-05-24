4b95105e63d385df4df9896b9d78efb519315461
AntonioR <ruizcabrera186@gmail.com>
2023-05-24 14:08:17 +0200

    Añadidos cambios a la clase

--- src/EdadesApp.java
+++ src/EdadesApp.java
@@ -3,7 +3,7 @@ public class EdadesApp {
 
 	public static void main(String[] args) {
 		System.out.println("Esto esta bien hecho aunque no sepa hacer el código");
-
+		System.out.println("Yolanda me quedaban literalmente 6 minutos, no terminaba ni de broma");
 	}
 
 }
